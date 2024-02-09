3d507ca3cd24732b740e81ea5d0c242be8c42ea0
maxiamikel <maxloversist@gmail.com>
2024-02-09 14:22:56 -0300

    Initial API configuration. Database connection ready and the project folders structure

---
+++ src/main/java/com/maxi/places/placeserviceapi/PlaceServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.places.placeserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxi/places/placeserviceapi/PlaceServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.places.placeserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
